54ed1822479c90d307c7a0268b62a8b208b32b98
TS22082 <ts22082@gmail.com>
2020-02-23 15:50:07 -0800

    prep for deploy

---
+++ client/src/App.js
@@ -0,0 +1,19 @@
+import React, { useEffect } from "react";
+import "./App.css";
+import axios from "axios";
+
+function App() {
+  useEffect(() => {
+    axios
+      .get("/example")
+      .then(response => console.log(response))
+      .catch(err => console.log(err));
+  });
+  return (
+    <div className="App">
+      <h1>hello world</h1>
+    </div>
+  );
+}
+
+export default App;

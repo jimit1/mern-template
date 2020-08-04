9390e5deac1d76be84c62f425c4dbbcd7119d8b2
TS22082 <ts22082@gmail.com>
2020-08-04 16:48:12 -0700

    modify server

--- server.js
+++ server.js
@@ -20,6 +20,10 @@ if (process.env.NODE_ENV === "production") {
   // app.get("*", (req, res) => {
   //   res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   // });
+
+  app.get("*", (request, response) => {
+    response.sendFile(path.join(__dirname, "client/build", "index.html"));
+  });
 }
 
 app.use(express.urlencoded({ extended: true }));

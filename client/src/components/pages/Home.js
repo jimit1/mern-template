614e968c61c1d369a02b8b874b6f5ae17f77c680
TS22082 <ts22082@gmail.com>
2020-08-05 11:55:56 -0700

    create the layout for tasks

--- client/src/components/pages/Home.js
+++ client/src/components/pages/Home.js
@@ -4,12 +4,13 @@ const Home = () => {
   const [state, setState] = useState({ text: "" });
 
   const styles = {
-    container: {
-      paddingTop: "50px",
+    spaceTop: {
+      paddingTop: "30px",
     },
 
-    text: {
-      transform: "translate(0px, 15px )",
+    button: {
+      marginRight: "10px",
+      width: "80px",
     },
   };
 
@@ -25,28 +26,59 @@ const Home = () => {
   };
 
   return (
-    <div className="container" style={styles.container}>
+    <div className="container" style={styles.spaceTop}>
+      {/* form and input  */}
       <div className="row">
         <form className="col s12" onSubmit={handleSubmit}>
           <div className="row">
-            <div className="input-field col s10">
+            <div className="input-field col s12">
               <input
                 id="text"
                 type="text"
                 className="validate input-large"
-                style={styles.text}
                 name={"text"}
                 onChange={handleChange}
               />
               <label htmlFor="text">Text</label>
             </div>
-            <div className="input-field col s2">
-              <button className="btn-floating btn-large waves-effect waves-light">
-                <i className="material-icons">add</i>
+          </div>
+          <div className="input-field">
+            <button className="btn waves-effect waves-light right">
+              Submit
+            </button>
+          </div>
+        </form>
+      </div>
+
+      {/* Tasks display */}
+
+      <div className="row" style={styles.spaceTop}>
+        <div class="card ">
+          <div class="card-content">
+            <span class="card-title">Card Title</span>
+            <p>
+              I am a very simple card. I am good at containing small bits of
+              information. I am convenient because I require little markup to
+              use effectively.
+            </p>
+          </div>
+          <div class="card-action">
+            <div className="input-field">
+              <button
+                className="btn waves-effect waves-light green"
+                style={styles.button}
+              >
+                edit
+              </button>
+              <button
+                className="btn waves-effect waves-light red"
+                style={styles.button}
+              >
+                delete
               </button>
             </div>
           </div>
-        </form>
+        </div>
       </div>
     </div>
   );

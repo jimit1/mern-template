f8b856daa0547e064dcb4450d0da8226d940f4d1
TS22082 <ts22082@gmail.com>
2020-08-05 11:22:12 -0700

    set up onchange and onsubmit functionality

--- client/src/components/pages/Home.js
+++ client/src/components/pages/Home.js
@@ -1,6 +1,8 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Home = () => {
+  const [state, setState] = useState({ text: "" });
+
   const styles = {
     container: {
       paddingTop: "50px",
@@ -10,24 +12,37 @@ const Home = () => {
       transform: "translate(0px, 15px )",
     },
   };
+
+  const handleChange = (e) => {
+    const text = e.target.value;
+    setState({ ...state, text });
+    console.log(state);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("hello");
+  };
+
   return (
     <div className="container" style={styles.container}>
       <div className="row">
-        <form className="col s12">
+        <form className="col s12" onSubmit={handleSubmit}>
           <div className="row">
             <div className="input-field col s10">
               <input
                 id="text"
                 type="text"
-                class="validate input-large"
+                className="validate input-large"
                 style={styles.text}
+                onChange={handleChange}
               />
-              <label for="text">Text</label>
+              <label htmlFor="text">Text</label>
             </div>
             <div className="input-field col s2">
-              <a className="btn-floating btn-large waves-effect waves-light">
+              <button className="btn-floating btn-large waves-effect waves-light">
                 <i className="material-icons">add</i>
-              </a>
+              </button>
             </div>
           </div>
         </form>

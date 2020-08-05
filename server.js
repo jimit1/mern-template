b38a5085a07910c8a62d729741bd37eea58ec34f
TS22082 <ts22082@gmail.com>
2020-08-04 17:06:24 -0700

    finished server

--- server.js
+++ server.js
@@ -17,13 +17,6 @@ mongoose
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
-  // app.get("*", (req, res) => {
-  //   res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-  // });
-
-  // app.get("*", (request, response) => {
-  //   response.sendFile(path.join(__dirname, "client/build", "index.html"));
-  // });
 }
 
 app.use(express.urlencoded({ extended: true }));

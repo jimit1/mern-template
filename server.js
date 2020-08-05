bad3d29f48030b0cc08f9bbe629c351fa91bbc76
TS22082 <ts22082@gmail.com>
2020-08-04 17:42:45 -0700

    remove comments

--- server.js
+++ server.js
@@ -2,18 +2,6 @@ const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 5000;
 require("./models/connection");
-// const mongoose = require("mongoose");
-// const MONGODB_URI =
-//   process.env.MONGODB_URI || "mongodb://localhost/mern_template";
-
-// mongoose
-//   .connect(MONGODB_URI, {
-//     useNewUrlParser: true,
-//     useUnifiedTopology: true,
-//     useFindAndModify: false,
-//   })
-//   .then(() => console.log("you are connected to mongodb"))
-//   .catch((err) => console.log(err));
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));

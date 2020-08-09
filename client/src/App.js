87cabb1c97bb8347a14c03de94a4ae4335a976bf
TS22082 <ts22082@gmail.com>
2020-08-09 15:11:48 -0700

    add paths

--- client/src/App.js
+++ client/src/App.js
@@ -3,10 +3,11 @@ import "./App.css";
 import axios from "axios";
 
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
-import Edit from "./components/Pages/Edit";
-import Delete from "./components/Pages/Delete";
-import Home from "./components/Pages/Home";
-import Nav from "./components/Nav/Nav";
+
+import Edit from "./components/Pages/Edit.js";
+import Delete from "./components/Pages/Delete.js";
+import Home from "./components/Pages/Home.js";
+import Nav from "./components/Nav/Nav.js";
 
 function App() {
   const [tasks, setTasks] = useState();
